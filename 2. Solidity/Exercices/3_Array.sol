f92777b52d1f6b2900a0ebfcc259a2e4e0489b4f
qbuxman <qbuxman@corp.free.fr>
2025-10-03 17:19:28 +0200

    exercice array

--- 2. Solidity/Exercices/3_Array.sol
+++ 2. Solidity/Exercices/3_Array.sol
@@ -4,22 +4,10 @@ pragma solidity 0.8.30;
 
 contract People {
 
-    Person public moi;
-
     struct Person {
         string name;
         uint age;
     }
 
-    function modifyPerson(string memory _name, uint _age) public  {
-        moi.name = _name;
-        moi.age = _age;
-
-        /*
-        * Autre solution possible
-        *
-        * moi = Person(_name, _age);
-        *
-        */
-    }
+    Person[] public persons;
 }
