e01565da24997241fe159e7d106ac702b84faa02
qbuxman <qbuxman@corp.free.fr>
2025-10-03 20:09:03 +0200

    exercice whitelist 3/5

---
+++ 2. Solidity/Exercices/7_Withelist_3.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.30;
+
+contract Whitelist {
+
+    mapping (address => bool) whitelist;
+
+    event Authorized(address _address);
+
+    function authorize(address _address) public {
+        whitelist[_address] = true;
+        emit Authorized(_address);
+    }
+}
