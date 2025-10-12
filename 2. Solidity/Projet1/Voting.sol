569fcaa8c3aa6cfff02bae0d0d52d5a9ff4bcbba
qbuxman <qbuxman@corp.free.fr>
2025-10-12 17:57:21 +0200

    finishing workflow

--- 2. Solidity/Projet1/Voting.sol
+++ 2. Solidity/Projet1/Voting.sol
@@ -31,22 +31,27 @@ contract Voting is Ownable {
     event Voted (address voter, uint proposalId);
 
     WorkflowStatus public status;
-
-    mapping (address => bool) authorizedVoter;
+    mapping (address => Voter) public authorizedVoters;
     Proposal[] private proposals;
 
     constructor() Ownable(msg.sender) {}
 
-    function getVoteStatus() external view returns(WorkflowStatus) {
-        return status;
+    modifier hasCorrectWorkflowStatus(WorkflowStatus _requiredStatus) {
+        require(status == _requiredStatus, "This functionality is not available");
+        _;
     }
 
-    function setWithlist(address _newVoterAdress) onlyOwner external {
-        require(status == WorkflowStatus.RegisteringVoters, "This functionnality is not available");
-        authorizedVoter[_newVoterAdress] = true;
-        emit VoterRegistered(_newVoterAdress);
+    modifier hasProposalsAvailable() {
+        require(proposals.length > 0, "No proposals available");
+        _;
     }
 
+    modifier senderIsRegistered() {
+        require(authorizedVoters[msg.sender].isRegistered, "You're not authorized");
+        _;
+    }
+
+    // Utilitary 
     function changeWorkflowStatus(WorkflowStatus _newStatus) onlyOwner external {
         require(_newStatus <= WorkflowStatus.VotesTallied, "This status does not exist");
         require(uint8(_newStatus) == uint8(status) + 1, "The voting status cannot be changed until the previous status has been completed");
@@ -57,9 +62,18 @@ contract Voting is Ownable {
         emit WorkflowStatusChange(_currentVoteStatus, _newStatus);
     }
 
-    function setProposal(string memory _description) external {
-        require(status == WorkflowStatus.ProposalsRegistrationStarted, "Setting proposal is not available");
-        require(authorizedVoter[msg.sender], "You're not authorized to send proposal");
+    // Step 1 - The owner set voters in a whitelist 
+    function setWhitelist(address _newVoterAddress) hasCorrectWorkflowStatus(WorkflowStatus.RegisteringVoters) onlyOwner external {
+        require(_newVoterAddress != address(0), "Invalid address");
+        require(!authorizedVoters[_newVoterAddress].isRegistered, "Voter already registered");
+        
+        authorizedVoters[_newVoterAddress] = Voter(true, false, 0);
+        
+        emit VoterRegistered(_newVoterAddress);
+    }
+
+    // Step 2 - The voters can submit proposal
+    function setProposal(string memory _description) hasCorrectWorkflowStatus(WorkflowStatus.ProposalsRegistrationStarted) senderIsRegistered external {
         require(bytes(_description).length > 0, "Description cannot be empty");
 
         proposals.push(
@@ -69,15 +83,44 @@ contract Voting is Ownable {
             })
         );
 
-        emit ProposalRegistered(proposals.length);
+        emit ProposalRegistered(proposals.length - 1);
     }
 
-    function getProposals() external view returns(Proposal[] memory) {
+    function getProposals() hasProposalsAvailable external view returns(Proposal[] memory) {
         return proposals;
     }
 
-    function getWinner() external view returns (address) {
+    // Step 3 - Only addresses in whitelist can vote
+    function submitVote(uint _proposalId) hasCorrectWorkflowStatus(WorkflowStatus.VotingSessionStarted) senderIsRegistered external {
+        require(!authorizedVoters[msg.sender].hasVoted, "You have already voted");
+        require(_proposalId < proposals.length, "Invalid proposal ID");
+
+        authorizedVoters[msg.sender].hasVoted = true;
+        authorizedVoters[msg.sender].votedProposalId = _proposalId;
+        proposals[_proposalId].voteCount++;
         
+        emit Voted(msg.sender, _proposalId);
     }
 
+    function getVoteByVoter(address _voterAddress) external view returns (Voter memory) {
+        require(authorizedVoters[_voterAddress].isRegistered, "Unknown voter");
+        
+        return authorizedVoters[_voterAddress];
+    }
+
+    // Step 4 - Get the winning proposal (max voteCount)
+    // Note: In case of a tie, returns the first proposal with the highest vote count
+    function getWinner() hasCorrectWorkflowStatus(WorkflowStatus.VotesTallied) hasProposalsAvailable external view returns (uint) {        
+        uint maxVotes;
+        uint winningProposalId;
+        
+        for (uint i = 0; i < proposals.length; i++) {
+            if (proposals[i].voteCount > maxVotes) {
+                maxVotes = proposals[i].voteCount;
+                winningProposalId = i;
+            }
+        }
+
+        return winningProposalId;
+    }
 }
