5c2dd2b72e36abde33b9ab79e52b8d09e246fb95
qbuxman <qbuxman@corp.free.fr>
2025-10-26 16:36:59 +0100

    ajout des premiers tests

--- 4. Testing/Projet2/test/Voting.ts
+++ 4. Testing/Projet2/test/Voting.ts
@@ -9,7 +9,7 @@ describe("Voting", function () {
     let firstVoter: any;
     let secondVoter: any;
 
-    beforeEach(async () => {
+    before(async () => {
         contract = await ethers.deployContract("Voting");
         [owner, firstVoter, secondVoter] = await ethers.getSigners();
     })
@@ -39,6 +39,28 @@ describe("Voting", function () {
             expect(voterData.isRegistered).to.be.false;
             expect(voterData.hasVoted).to.be.false; 
             expect(voterData.votedProposalId).to.eq(0n);
+        });
+    })
+
+    describe("Getters", () => {
+        it("Should return a voter with getVoter", async () => {
+            await contract.addVoter(firstVoter.address)
+
+            expect(await contract.connect(firstVoter).getVoter(firstVoter.address)).to.deep.eq([true, false, 0n]);
+        });
+
+        it.only("Should return a default proposal with getOneProposal", async () => {
+            await contract.addVoter(firstVoter.address)
+            await contract.startProposalsRegistering()
+
+            expect(await contract.connect(firstVoter).getOneProposal(0)).to.deep.eq(['GENESIS', 0n]);
+        });
+
+        it.only("Should return a proposal with getOneProposal after ", async () => {
+            await contract.addVoter(firstVoter.address)
+            await contract.startProposalsRegistering()
+
+            expect(await contract.connect(firstVoter).getOneProposal(0)).to.deep.eq(['GENESIS', 0n]);
         });
     })
 });
